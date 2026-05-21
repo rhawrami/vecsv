eb3edd2af061700e91712ab3938adb9a3927d35c
r h <ravanhawrami@gmail.com>
2026-05-20 22:09:44 -0500

    return quote state and N offsets read

--- internal/simd/scan.arm64.go
+++ internal/simd/scan.arm64.go
@@ -2,24 +2,27 @@
 
 package simd
 
-// ScanForStructuralChars scans a byte slice for the following characters:
-//
-// 1. newline:    `\n`
-// 2. separator:  `,|\t|;...`
-// 3. quotes:     `"`
-//
-// When such a character is found... blah blah
-//
-//go:noescape
-func ScanForStructuralChars(b []byte, sep byte)
+// ResCMRE defines the results returned from a call to _compare_mask_reduce_extract.
+type resCMRE struct {
+	inQuotes    int // 1 if left off in a quote pair; 0 otherwise
+	offsetsRead int // number of offsets recorded
+}
 
 // _compare_mask_reduce_extract compares bytes in `b` to the following:
-//
 // 1. newline:    `\n`
 // 2. separator:  `,|\t|;...`
 // 3. quotes:     `"`
+//
+// For each 64-byte chunk, two 64-bit masks are built: a newline|sep identifier, and
+// a quote identifier; `in_quotes` determines if the starting chunk was left off in a
+// quote pair; this information is used to control for newline|sep characters found
+// in quote pairs; once these characters are masked out, offsets corresponding to newlin_sep
+// characters are written out to `o`; `at` is used as the base offset for the first byte in `b`.
+//
+// A resCMRE result is returned, containing the quote state, and the number of offsets read, in order
+// to update/grow the offsets buffer.
 //
 // Assumes length of `b` is divisible by 64.
 //
 //go:noescape
-func C_compare_mask_reduce_extract(b []byte, o []uint32, in_quotes int, sep byte) int
+func _compare_mask_reduce_extract(b []byte, o []uint32, in_quotes, at int, sep byte) resCMRE
--- internal/simd/scan.arm64.s
+++ internal/simd/scan.arm64.s
@@ -2,14 +2,9 @@
 
 #include "textflag.h"
 
-#define QUOTE_CHAR $34
-#define LF_CHAR $10
-
-#define B_MASK_0 $0x1001100110011001
-#define B_MASK_1 $0x2002200220022002
-#define B_MASK_2 $0x4004400440044004
-#define B_MASK_3 $0x8008800880088008
-
+// PREFIX_XOR calculate the prefix XOR sum of `target_gp`
+// uses `scratch_gp` in the process, and overwrites `target_gp`
+// with the result.
 #define PREFIX_XOR(target_gp, scratch_gp)    \
         RBIT target_gp, target_gp            \
         LSR $1, target_gp, scratch_gp        \
@@ -26,113 +21,154 @@
         EOR target_gp, scratch_gp, target_gp \
         RBIT target_gp, target_gp
 
-#define POP_COUNT(target_gp, out_gp)   \ 
-        VEOR V0.B16, V0.B16, V0.B16    \
-        VMOV target_gp, V1.D[0]        \
-        VCNT V1.B8, V1.B8              \
-        VADDV V1.B8, V0                \
+// POP_COUNT calculates the population count of the 64 bits 
+// in `target_gp`, placing the result in `out_gp`.
+#define POP_COUNT(target_gp, out_gp) \ 
+        VEOR V0.B16, V0.B16, V0.B16  \
+        VMOV target_gp, V1.D[0]      \
+        VCNT V1.B8, V1.B8            \
+        VADDV V1.B8, V0              \
         VMOV V0.D[0], out_gp
 
-#define CMP_MASK_REDUCE(target, out_gp)   \
-        VCMEQ target.B16, V0.B16, V11.B16 \
-        VCMEQ target.B16, V1.B16, V12.B16 \
-        VCMEQ target.B16, V2.B16, V13.B16 \
-        VCMEQ target.B16, V3.B16, V14.B16 \
-        VAND V11.B16, V4.B16, V11.B16     \
-        VAND V12.B16, V5.B16, V12.B16     \
-        VAND V13.B16, V6.B16, V13.B16     \
-        VAND V14.B16, V7.B16, V14.B16     \
-        VADD V11.D2, V12.D2, V11.D2       \
-        VADD V13.D2, V14.D2, V12.D2       \
-        VADD V11.D2, V12.D2, V11.D2       \
-        VADDP V11.B16, V11.B16, V11.B16   \
+// CMP_MASK_REDUCE calculates the 64-bit mask of whether bytes 
+// in [A_V, B_V, C_V, D_V] are equal to the values in `target`,
+// placing the resulting mask in `out_gp`. 
+#define CMP_MASK_REDUCE(target, out_gp)    \
+        VCMEQ target.B16, A_V.B16, V11.B16 \
+        VCMEQ target.B16, B_V.B16, V12.B16 \
+        VCMEQ target.B16, C_V.B16, V13.B16 \
+        VCMEQ target.B16, D_V.B16, V14.B16 \
+        VAND V11.B16, M0_V.B16, V11.B16    \
+        VAND V12.B16, M1_V.B16, V12.B16    \
+        VAND V13.B16, M2_V.B16, V13.B16    \
+        VAND V14.B16, M3_V.B16, V14.B16    \
+        VADD V11.D2, V12.D2, V11.D2        \
+        VADD V13.D2, V14.D2, V12.D2        \
+        VADD V11.D2, V12.D2, V11.D2        \
+        VADDP V11.B16, V11.B16, V11.B16    \
         VMOV V11.D[0], out_gp
 
+// CMP_MASK_REDUCE_2 calculates the 64-bit mask of whether bytes 
+// in [A_V, B_V, C_V, D_V] are equal to the values in `target1` 
+// or `target2`, placing the resulting mask in `out_gp`. 
 #define CMP_MASK_REDUCE_2(target1, target2, out_gp) \
-        VCMEQ target1.B16, V0.B16, V11.B16          \
-        VCMEQ target1.B16, V1.B16, V12.B16          \
-        VCMEQ target1.B16, V2.B16, V13.B16          \
-        VCMEQ target1.B16, V3.B16, V14.B16          \
-        VCMEQ target2.B16, V0.B16, V15.B16          \
-        VCMEQ target2.B16, V1.B16, V16.B16          \
-        VCMEQ target2.B16, V2.B16, V17.B16          \
-        VCMEQ target2.B16, V3.B16, V18.B16          \
+        VCMEQ target1.B16, A_V.B16, V11.B16         \
+        VCMEQ target1.B16, B_V.B16, V12.B16         \
+        VCMEQ target1.B16, C_V.B16, V13.B16         \
+        VCMEQ target1.B16, D_V.B16, V14.B16         \
+        VCMEQ target2.B16, A_V.B16, V15.B16         \
+        VCMEQ target2.B16, B_V.B16, V16.B16         \
+        VCMEQ target2.B16, C_V.B16, V17.B16         \
+        VCMEQ target2.B16, D_V.B16, V18.B16         \
         VORR V11.B16, V15.B16, V11.B16              \
         VORR V12.B16, V16.B16, V12.B16              \
         VORR V13.B16, V17.B16, V13.B16              \
         VORR V14.B16, V18.B16, V14.B16              \
-        VAND V11.B16, V4.B16, V11.B16               \
-        VAND V12.B16, V5.B16, V12.B16               \
-        VAND V13.B16, V6.B16, V13.B16               \
-        VAND V14.B16, V7.B16, V14.B16               \
+        VAND V11.B16, M0_V.B16, V11.B16             \
+        VAND V12.B16, M1_V.B16, V12.B16             \
+        VAND V13.B16, M2_V.B16, V13.B16             \
+        VAND V14.B16, M3_V.B16, V14.B16             \
         VADD V11.D2, V12.D2, V11.D2                 \
         VADD V13.D2, V14.D2, V12.D2                 \
         VADD V11.D2, V12.D2, V11.D2                 \
         VADDP V11.B16, V11.B16, V11.B16             \
         VMOV V11.D[0], out_gp
 
-// func _compare_mask_reduce_extract(b []byte, o []uint32, in_quotes int, sep byte) int
-TEXT ·C_compare_mask_reduce_extract(SB),NOSPLIT,$0-72
-    MOVD b+0(FP), R0  
-    MOVD o+24(FP), R1  
-    MOVD N+8(FP), R2  
-    EOR R3, R3
+#define QUOTE_CHAR $34
+#define LF_CHAR $10
 
-    MOVD LF_CHAR, R4                                 // init `\n`
-    VDUP R4, V8.B16
+#define B_MASK_0 $0x1001100110011001
+#define B_MASK_1 $0x2002200220022002
+#define B_MASK_2 $0x4004400440044004
+#define B_MASK_3 $0x8008800880088008
 
-    MOVD SEP_CHAR+56(FP), R4                         // init `sep`
-    VDUP R4, V9.B16
+#define BASE_R R0
+#define OFF_R R1
+#define LEN_R R2
+#define CTR_R R3
+#define AT_R R10
 
-    MOVD QUOTE_CHAR, R4                              // init `"`
-    VDUP R4, V10.B16
+#define QSTATE_R R4
+#define NOFFSETSREAD_R R9
 
-    MOVD in_quotes+48(FP), R4
+#define A_V V0
+#define B_V V1
+#define C_V V2
+#define D_V V3
 
-    VMOVQ B_MASK_0, B_MASK_0, V4
-    VMOVQ B_MASK_1, B_MASK_1, V5
-    VMOVQ B_MASK_2, B_MASK_2, V6
-    VMOVQ B_MASK_3, B_MASK_3, V7
+#define M0_V V4
+#define M1_V V5
+#define M2_V V6
+#define M3_V V7
 
-    CMP $0, R2
+#define LF_V V8
+#define SEP_V V9
+#define QUOTE_V V10
+
+// func _compare_mask_reduce_extract(b []byte, o []uint32, in_quotes, at int, sep byte) resCMRE
+TEXT ·_compare_mask_reduce_extract(SB),NOSPLIT,$0-96
+    MOVD b+0(FP), BASE_R
+    MOVD o+24(FP), OFF_R  
+    MOVD N+8(FP), LEN_R  
+    MOVD in_quotes+48(FP), QSTATE_R
+    MOVD at+56(FP), AT_R
+    EOR NOFFSETSREAD_R, NOFFSETSREAD_R
+    EOR CTR_R, CTR_R
+
+    MOVD LF_CHAR, R5                          
+    VDUP R5, LF_V.B16
+    MOVD SEP_CHAR+56(FP), R5                         
+    VDUP R5, SEP_V.B16
+    MOVD QUOTE_CHAR, R5                              
+    VDUP R5, QUOTE_V.B16
+
+    VMOVQ B_MASK_0, B_MASK_0, M0_V
+    VMOVQ B_MASK_1, B_MASK_1, M1_V
+    VMOVQ B_MASK_2, B_MASK_2, M2_V
+    VMOVQ B_MASK_3, B_MASK_3, M3_V
+
+    CMP $0, LEN_R
     BEQ exit_fn 
 
 cmp_mask_loop:
-    VLD4.P 64(R0), [V0.B16, V1.B16, V2.B16, V3.B16]
-    CMP_MASK_REDUCE_2(V8, V9, R6)                     // Vx_i == `\n` | Vx_i == `sep` ? 0xFF : 0x00  
-    CMP_MASK_REDUCE(V10, R7)                          // Vx_i == `"` ? 0xFF : 0x00
+    VLD4.P 64(BASE_R), [A_V.B16, B_V.B16, C_V.B16, D_V.B16]
 
-    ORR R4, R7, R7
+    CMP_MASK_REDUCE_2(LF_V, SEP_V, R5)                     
+    CMP_MASK_REDUCE(QUOTE_V, R6)        
 
-    PREFIX_XOR(R7, R8)
-    LSR $63, R7, R4
-    BIC R7, R6, R6
+    ORR QSTATE_R, R6, R6
 
+    PREFIX_XOR(R6, R8)
+    LSR $63, R6, QSTATE_R
 
-    POP_COUNT(R6, R11)
-    CMP $0, R11
+    BIC R6, R5, R5
+
+    POP_COUNT(R5, R8)
+    ADD R8, NOFFSETSREAD_R
+    CMP $0, R8
     BEQ eval
 
-    RBIT R6, R6
-    MOVD $0x7FFFFFFFFFFFFFFF, R7
+    RBIT R5, R5
+    MOVD $0x7FFFFFFFFFFFFFFF, R6
 extract_loop:
-    CLZ R6, R9
-    ADD R3, R9, R10
-    MOVW.P R10, 4(R1)
+    CLZ R5, R7
+    ADD AT_R, R7, R11
+    MOVW.P R11, 4(R1)
 
-    LSR R9, R7, R9
-    AND R9, R6, R6
+    LSR R7, R6, R7
+    AND R7, R5, R5
 
-    SUB $1, R11
-    CMP $0, R11
+    SUB $1, R8
+    CMP $0, R8
     BGT extract_loop
 
 eval:
-    ADD $64, R3
-    CMP R2, R3
+    ADD $64, AT_R
+    ADD $64, CTR_R
+    CMP LEN_R, CTR_R
     BLT cmp_mask_loop
 
 exit_fn:
-    MOVD R4, out+64(FP)
+    MOVD QSTATE_R, quote_state_out+64(FP)
+    MOVD NOFFSETSREAD_R, n_offsets_read_out+72(FP)
     RET
-
