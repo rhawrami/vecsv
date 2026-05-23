e29edb92221bd4637a3ea3fd20142a0ca1929e27
r h <ravanhawrami@gmail.com>
2026-05-23 02:12:32 -0500

    add stubs for amd64

---
+++ internal/parser/scan.amd64.go
@@ -0,0 +1,24 @@
+//go:build amd64
+
+package parser
+
+import "golang.org/x/sys/cpu"
+
+var (
+	compareMaskReduceExtract func(b []byte, o []uint, in_quotes, at int, sep byte) resCMRE
+)
+
+func init() {
+	if cpu.X86.HasAVX2 {
+		compareMaskReduceExtract = cmreAVX2
+	} else {
+		// SSE2 is always available on amd64
+		compareMaskReduceExtract = cmreSSE2
+	}
+}
+
+//go:noescape
+func cmreAVX2(b []byte, o []uint, in_quotes, at int, sep byte) resCMRE
+
+//go:noescape
+func cmreSSE2(b []byte, o []uint, in_quotes, at int, sep byte) resCMRE
